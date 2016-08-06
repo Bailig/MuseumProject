14f93f190f3915de9a3a66641140c4c8391c77a3
Bailig Abhanar <barrybailig@hotmail.com>
2016-08-06 03:03:54 -0400

    documentation for technician compeleted

--- component/js/colorbox.js
+++ component/js/colorbox.js
@@ -1,4 +1,4 @@
-$('#usage .row').find('a').colorbox({
+$('#usage .row, #technician .col-md-4, #technician .col-md-2, #technician #overview').find('a').colorbox({
     rel:'gal',
     width: '55%'
 });
